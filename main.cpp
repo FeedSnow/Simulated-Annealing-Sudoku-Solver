4d91dd6d75e720831784ec74dc268e2b687c7fe9
FeedSnow <mateovip22262@gmail.com>
2024-09-17 10:02:29 +0200

    old data is cleaned before use

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <filesystem>
 #include "sudoku.h"
 #include "reader.h"
 
 using namespace std;
+namespace fs = filesystem;
 
 int main()
 {
+	if (fs::exists("data.txt"))
+		fs::remove("data.txt");
+
 	int temp[81];
 	Read(temp);
 	
 	Sudoku s(temp);
 	s.Solve();
+
+	cout << "Enter any character to exit." << endl;
+	char ch;
+	cin >> ch;
 
 	return 0;
 }
